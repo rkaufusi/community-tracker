ac93f35b178f19ff232ba45b72a55d2dd7029879
rkaufusi <rhettkaufusi@gmail.com>
2021-09-12 08:57:45 -0700

    added labels to info

--- src/Info.js
+++ src/Info.js
@@ -71,7 +71,6 @@ const clicked = (name, market_rank, image, symbol, current_price, market_cap) =>
     console.log(modal);
 }
 
-
     const fetchReddit = async () => {
         try {
             const response = await fetch(urlReddit);
@@ -98,17 +97,19 @@ const filteredCoins = currency.filter(coin => coin.name.toLowerCase().includes(s
 console.log("filtered " + filteredCoins.length);
 
 const loadMore = () => {
-  setAmount(amount + 10)
+  setAmount(amount + 10);
 }
     return (
         <div>
           <h3>Crypto Community Tracker</h3>
           <input type='text' placeholder='search here' onChange={handleChange} value={searchInput} placeholder='Search'/>
+          <div className='crypto-row'>
+          </div>
           
             {filteredCoins.slice(0, amount).map((val) => {
               return ( 
                 <>
-
+                
                   <div className='crypto-container clickable' onClick={ () => clicked(
                     val.name, 
                     val.market_cap_rank, 
@@ -118,31 +119,35 @@ const loadMore = () => {
                     val.market_cap
                     )
                     }>
-                      {columnHeader.forEach((val) => val)}
+
                       <p className='crypto-row'>{val.market_cap_rank}</p>
                       <p className='crypto-row'>
                         <img className='crypto-img' src={val.image} alt={val.name}/>
                       </p>
                       <p className='crypto-row'>{val.name}</p>
                       <p className='crypto-row'>{val.symbol}</p>
-                      <p className='crypto-row'>{formatter.format(val.current_price)}</p>
-                      <p className='crypto-row'>{formatter.format(val.market_cap)}</p>
+                      <p className='crypto-row'>{'Price: ' + formatter.format(val.current_price)}</p>
+                      <p className='crypto-row'>{'Mkt Cap: ' + formatter.format(val.market_cap)}</p>
                         </div>                                             
                           {modalOpen && (
                             <div className='modal-overlay'>
+                            
                               <div className='modal-container'>
-                                <p className='modal-row'>{modal[0]}</p>
                                 <p className='modal-row'>{modal[1]}</p>
+                                <p className='modal-row'>{modal[0]}</p>
                                 <p>
                                   <img className='crypto-img' src={modal[2]} alt={modal[0]}/>
                                 </p>
                                 <p>{modal[3]}</p>
-                                <p>{modal[4]}</p>
-                                <p>{modal[5]}</p>
-                                <p>{myLatestState}</p>
-                                <a href={modal[6]} target='_blank'>
+                                <p>Price <br/> {modal[4]}</p>
+                                <p>Market Cap <br/> {modal[5]}</p>
+                                <p>Reddit subs <br/> {myLatestState}</p>
+                                <p> Reddit <br/> 
+                                  <a href={modal[6]} target='_blank'>
                                     <img className='crypto-img' src={redditLogo} alt='reddit'/>
-                                </a>
+                                  </a>
+                                </p>
+
 
                                 <button className='close-modal-btn' onClick={() => setModalOpen(false)}>
                                   X
